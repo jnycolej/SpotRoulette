f113f718b117ccd7138343a16ddaf34950d5e2b4
jnycolej <98425262+jnycolej@users.noreply.github.com>
2023-05-30 14:07:04 -0500

    Added Lists

--- public/scripts/models/spotrou.model.js
+++ public/scripts/models/spotrou.model.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const { getRandomDestination: getRandomDestinationFromUtils } = require('../destinationUtils');
 const path = require('path');
 
+const getDataFromJSONFull = () => {
+  const data = require('../../../places.json');
+  console.log(data); // Check if data is correctly retrieved
+  return data;
+};
+
 function getDataFromJSON(id) {
-    //const filePath = path.resolve(__dirname, '../../places.json');
-    //const data = fs.readFileSync(filePath);
-    //const jsonData = JSON.parse(data);
-    //return jsonData;
     const jsonData = require('../../../places.json');
-    //const destination = jsonData.find(item => item.id === id);
     const destination = jsonData.find((item) => item.id.toString() === id);
     return destination;
 }
@@ -22,5 +23,6 @@ function getRandomDestination() {
 
 module.exports = {
     getRandomDestination: getRandomDestinationFromUtils,
-    getDataFromJSON
+    getDataFromJSON,
+    getDataFromJSONFull
 };
--- public/scripts/routes/spotrou.routes.js
+++ public/scripts/routes/spotrou.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //const controllers = require('../controllers/spotrou.controllers');
-const { getDataFromJSON } = require('../models/spotrou.model');
+const { getDataFromJSON, getDataFromJSONFull } = require('../models/spotrou.model');
 const { getRandomDestination} = require('../destinationUtils');
 
 //router.get('/random-destination', controllers.getRandomDestination);
@@ -14,10 +14,8 @@ router.get('/random-destination', (req, res) => {
 router.get('/destination/:id', async (req, res) => {
   const id = req.params.id;
   try {
-    //const destination = await getDataFromJSON(id);
-    //const destination = await getRandomDestination(id);
     const destination = getDataFromJSON(id);
-    console.log(destination);
+    //console.log(destination);
     res.render('more-info', { destination: destination });
   } catch (error) {
     console.error(error);
@@ -26,13 +24,11 @@ router.get('/destination/:id', async (req, res) => {
 });
 
 router.get('/categoryList', async (req, res) => {
-  const category = req.query.category;
-
   try {
-    const data = await getDataFromJSON(); // Assuming getDataFromJSON returns the JSON data
-    const filteredData = data.filter(item => item.category === category);
-
-    res.render('list', { category, data: filteredData });
+    const data = await getDataFromJSONFull(); // Assuming getDataFromJSON returns the JSON data
+    const category = req.query.category;
+    const filteredData = data.filter(item => item.category === 'restaurant');
+    res.render('categoryList', {category, data: filteredData});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
