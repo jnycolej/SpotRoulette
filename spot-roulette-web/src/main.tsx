e8ef31b8bfbf0f8b9e0f247bc8799165e415a514
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-05-14 15:45:33 -0500

    Added a new page route

--- spot-roulette-web/src/main.tsx
+++ spot-roulette-web/src/main.tsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Random from './pages/Random';
 import SpotDetail from './pages/SpotDetail';
 import SpotList from './pages/SpotList';
+import FilteredSpotRoulette from './pages/FilteredSpotRoulette';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/random" element={<Random />} />
         <Route path="/places/:id" element={<SpotDetail />} />
         <Route path="/places" element={<SpotList />} />
+        <Route path="/places/FilteredSpotRoulette" element={<FilteredSpotRoulette />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
