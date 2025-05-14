88795673262c93a05331dc3955613b13d916bfb3
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-05-14 15:55:58 -0500

    Updated the amount of information displayed in list table

--- spot-roulette-web/src/pages/SpotList.tsx
+++ spot-roulette-web/src/pages/SpotList.tsx
@@ -1,22 +1,20 @@
 import { useState, useEffect } from "react";
 import NavBar from '../components/Navbar';
+import { Navigation, useNavigate } from "react-router-dom";
+
 
 export default function SpotList() {
     
     const [places, setPlaces] = useState<any[]>([]);
-    
-    function getNeighborhood(zipcode: string) {
-        switch(zipcode) {
-             
-        }
-    }
+    const navigate  = useNavigate();
+
     async  function fetchList() {
         try {
             const res = await fetch("/api/places");
             if (res.ok) {
                 const data = await res.json();
                 setPlaces(data);
-                console.log(data);  
+                // console.log(data);  
             } else {
                 throw new Error('Failed to fetch data');
             }
@@ -40,20 +38,28 @@ export default function SpotList() {
                     <thead>
                         <tr>
                             <th>Name</th>
+                            <th>Neighborhood</th>
                             <th>Type</th>
-                            <th>Description</th>
+                            {/* <th>Description</th> */}
                             <th>Address</th>
                         </tr>
                     </thead>
                     <tbody>
+                        
                         {places.map((place, index) => (
+                            
                             <tr key={place.id || index}>
-                                <td>{place.name}</td>
+                                <button onClick={() => {navigate(`/places/${place.id}`)}}><td>{place.name}</td></button>
+                                <td>{Array.isArray(place.neighborhoods) 
+                                        ? place.neighborhoods?.join("/") 
+                                        : place.neighborhoods || "N/A"}</td>
                                 <td>{place.type}</td>
-                                <td>{place.description || "Not available yet"}</td>
-                                <td>{place.address}, {place.state}</td>
+                                {/* <td>{place.description || "Not available yet"}</td> */}
+                                <td>{place.address}, {place.state} {place.zipcode}</td>
                             </tr>
+                            
                         ))}
+                        
                     </tbody>
                 </table>
             </div>
